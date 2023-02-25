394f3d489145c1b9b3c35d2fb20612c7271c4483
weikie <weikie@gmail.com>
2023-02-25 15:50:49 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,54 @@
+LCD1IN8.LCD_Init()
+LCD1IN8.LCD_Clear()
+LCD1IN8.DrawCircle(
+80,
+64,
+45,
+LCD1IN8.Get_Color(LCD_COLOR.BLUE),
+DRAW_FILL.DRAW_EMPTY,
+DOT_PIXEL.DOT_PIXEL_2
+)
+LCD1IN8.DrawRectangle(
+10,
+10,
+150,
+118,
+LCD1IN8.Get_Color(LCD_COLOR.GREEN),
+DRAW_FILL.DRAW_EMPTY,
+DOT_PIXEL.DOT_PIXEL_3
+)
+LCD1IN8.LCD_Display()
+basic.forever(function () {
+    LCD1IN8.DrawCircle(
+    80,
+    64,
+    40,
+    LCD1IN8.Get_Color(LCD_COLOR.WHITE),
+    DRAW_FILL.DRAW_FULL,
+    DOT_PIXEL.DOT_PIXEL_1
+    )
+    LCD1IN8.DisNumber(
+    70,
+    40,
+    input.compassHeading(),
+    LCD1IN8.Get_Color(LCD_COLOR.BROWN)
+    )
+    LCD1IN8.DisString(
+    50,
+    75,
+    "Temp:",
+    LCD1IN8.Get_Color(LCD_COLOR.RED)
+    )
+    LCD1IN8.DisNumber(
+    85,
+    75,
+    input.temperature(),
+    LCD1IN8.Get_Color(LCD_COLOR.RED)
+    )
+    LCD1IN8.LCD_DisplayWindows(
+    50,
+    40,
+    120,
+    90
+    )
+})
